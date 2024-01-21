9b425ec54323efd0b29b77adaa01e8b4e9abf100
prashant jinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-01-21 14:30:53 +0530

    First commit

---
+++ PopUP/index.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById("popup")
+
+function openPop(){
+    popup.classList.add("open-popup")
+
+}
+function closePop(){
+    popup.classList.remove("open-popup")
+
+
+}
